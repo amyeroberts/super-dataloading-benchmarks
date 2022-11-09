e01c00e8e53c854043f3d18a831419349e3ba6ab
Quentin Lhoest <lhoest.q@gmail.com>
2022-11-09 17:41:53 +0100

    tqdm desc

--- prepare_imagefolder.py
+++ prepare_imagefolder.py
@@ -5,10 +5,10 @@ from tqdm import tqdm
 print("downloading 10k images")
 revision = "924f819d1060140090b89f187a935f61b0e2954c"  # for reproducibility
 imgnt = load_dataset("imagenet-1k", streaming=True, split="train", revision=revision).shuffle(seed=42)
-subset = list(imgnt.take(10_000))
+subset = list(tqdm(imgnt.take(10_000), total=10_000, desc="downloading"))
 
 print("saving 10k images to ./imagefolder")
 d = Path(__file__).resolve() / "imagefolder"
 d.mkdir(exist_ok=True)
-for i, x in enumerate(tqdm(subset, total=10_000)):
+for i, x in enumerate(tqdm(subset, total=10_000, desc="writing")):
     x["image"].save(d / f"{i:05d}.png")
