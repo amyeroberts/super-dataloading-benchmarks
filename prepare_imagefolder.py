b09d51a9f654da09fc94fd07201a91add8082560
Quentin Lhoest <lhoest.q@gmail.com>
2022-11-09 17:40:57 +0100

    add tqdm

--- prepare_imagefolder.py
+++ prepare_imagefolder.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 from pathlib import Path
+from tqdm import tqdm
 
 print("downloading 10k images")
 revision = "924f819d1060140090b89f187a935f61b0e2954c"  # for reproducibility
@@ -8,5 +9,6 @@ subset = list(imgnt.take(10_000))
 
 print("saving 10k images to ./imagefolder")
 d = Path(__file__).resolve() / "imagefolder"
-for i, x in enumerate(subset):
+d.mkdir(exist_ok=True)
+for i, x in enumerate(tqdm(subset, total=10_000)):
     x["image"].save(d / f"{i:05d}.png")
